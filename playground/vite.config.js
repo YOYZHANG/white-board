29174306ec8af52b8128e7980ce138ce4246acaf
= <=>
2023-08-08 14:46:08 +0800

    feat: init

---
+++ playground/vite.config.js
@@ -0,0 +1,14 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Unocss(),
+  ],
+})
